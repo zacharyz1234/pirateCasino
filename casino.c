63bdd2b902e23e3e33df77a9648c7f300d00ff47
zacharyz1234 <zakzeldin@gmail.com>
2025-05-03 18:19:17 -0400

    I added a lot today so prep them readers. Added 4 new arrays to combine the player's cards and the dealer's cards with the cards on the table. So four arrays of seven cards, two for player card no and suit and two for dealer card no and suit. Made a function to evaluate the hands. Im not going to go into specifics of each thing to evaluate, but it counts the amount of times a card of number i was used and adds it to an array to keep track of how many times each number was used. Same for suits. It then evaluates the hand type. Also added a new print function to print out the player, the table, and now the dealer as well. This print is called after final bet/stand. Started on a function to print who the winner is but I worked for a few hours so I am feeling a little burnt out. Will continue tmrw.

--- casino.c
+++ casino.c
@@ -110,6 +110,8 @@ void displayMenu(long long bank)
 
 //POKER==========================================================================================================
 
+
+
 void printCard(int number, int suit)
 {
     char *numStr;
@@ -184,6 +186,225 @@ void printPlayerAndTableCards(int playerCardNo[], int playerCardSuit[], int tabl
         printCard(tableCardNo[i], tableCardSuit[i]);
         printf("\n");
     }
+}
+
+void printAllCards(int playerCardNo[], int playerCardSuit[], int tableCardNo[], int tableCardSuit[], int tableCardsShown, int dealerCardNo[], int dealerCardSuit[])
+{
+    printf("\nYour Hand:\n");
+    for (int i = 0; i < 2; i++)
+    {
+        printf("- ");
+        printCard(playerCardNo[i], playerCardSuit[i]);
+        printf("\n");
+    }
+
+    printf("\nTable Cards:\n");
+    for (int i = 0; i < tableCardsShown; i++)
+    {
+        printf("- ");
+        printCard(tableCardNo[i], tableCardSuit[i]);
+        printf("\n");
+    }
+
+    printf("\nDealer Cards:\n");
+    for(int i = 0; i < 2; i++)
+    {
+        printf("- ");
+        printCard(dealerCardNo[i], dealerCardSuit[i]);
+        printf("\n");
+    }
+
+}
+
+int evaluateHands(int allCardNum[], int allCardSuit[])
+{
+    //Sorts the arrays so they are ordered from least to greatest. Makes it easier to check for a straight later on
+    //This is bubble sort
+    for (int i = 0; i < 7; i++) 
+    {
+
+        for (int j = i + 1; j < 7; j++)
+        {
+
+            if (allCardNum[i] > allCardNum[j]) \
+            {
+                // Swap card numbers
+                int tempNum = allCardNum[i];
+                allCardNum[i] = allCardNum[j];
+                allCardNum[j] = tempNum;
+    
+                // Swap card suits with the numbers
+                int tempSuit = allCardSuit[i];
+                allCardSuit[i] = allCardSuit[j];
+                allCardSuit[j] = tempSuit;
+            }
+        }
+    }
+
+    //Counts the amount of which cards and which suits are used
+    int numCount[13] = {0};
+    int suitCount[4] = {0};
+
+    //Subtracts 1 to correctly place the number in the right index
+    for(int i = 0; i < 7; i++)
+    {
+        numCount[allCardNum[i] - 1]++;
+        suitCount[allCardSuit[i] - 1]++;
+    }
+
+    //Checks for a flush
+    bool isFlush = false;
+    for(int i = 0; i < 4; i++)
+    {
+        if(suitCount[i] >= 5)
+        {
+            isFlush = true;
+            break;
+        }
+    }
+
+
+    //Checks for a straight
+    bool isStraight = false;
+    for(int i = 0; i < 3; i++) //Iterates 3 times because there are 7 elements, so there could only be 3 possible iterations of 5 in a row
+    {
+        if(allCardNum[i] + 1 == allCardNum[i + 1] && allCardNum[i + 1] + 1 == allCardNum[i + 2] && allCardNum[i + 2] + 1 == allCardNum[i + 3] && allCardNum[i + 3] + 1 == allCardNum[i + 4])
+        {
+            isStraight = true;
+            break;
+        }
+    }
+    //Checks for a straight with an Ace, 10, Jack, Queen, and King. They're out of order so I'm checking seperately
+    if(numCount[0] >= 1 && numCount[10] >= 1 && numCount[11] >= 1 && numCount[12] >= 1 && numCount[13] >= 1)
+    {
+        isStraight = true;
+
+    }
+
+    //Checks for the royal flush
+    //Loops through each suit
+    for (int i = 0; i < 4; i++) 
+    {
+        //Five booleans to check that the specific card values and suits all exist within a hand
+        bool has10 = false, hasJack = false, hasQueen = false, hasKing = false, hasAce = false;
+
+        //Loops through the 7 cards in the array
+        for (int j = 0; j < 7; j++) 
+        {
+            //If the current card suit is the suit we are looking at
+            if (allCardSuit[j] == i) 
+            {
+                if (allCardNum[j] == 1) 
+                {
+                    hasAce = true;
+                }
+                if (allCardNum[j] == 10)
+                {
+                    has10 = true;
+                } 
+                if (allCardNum[j] == 11) 
+                {
+                    hasJack = true;
+                }
+                if (allCardNum[j] == 12) 
+                {
+                    hasQueen = true; 
+                }
+                if (allCardNum[j] == 13) 
+                {
+                    hasKing = true;
+                }
+            }
+        }
+
+        //If there are these 5 cards all within the same suit then it is a royal flush
+        if (has10 && hasJack && hasQueen && hasKing && hasAce) 
+        {
+            return 10; // Royal Flush
+        }
+    }
+
+
+    //Check for straight flush
+    if(isFlush && isStraight)
+    {
+        return 9; //Straight Flush
+    }
+
+    //Check for four of a kind
+    for(int i = 0; i < 13; i++)
+    {
+        if(numCount[i] == 4)
+        {
+            return 8; //Four of a kind
+        }
+    }
+
+    //Check for full house
+    for(int i = 0; i < 13; i++)
+    {
+        if(numCount[i] == 3)
+        {
+            for(int j = i; j < 13; j++)
+            {
+                if(j != i)
+                {
+                    if(numCount[j] == 2)
+                    {
+                        return 7; //Full house
+                    }
+                }
+            }
+        }
+    }
+
+    //Return statement for flush
+    if(isFlush)
+    {
+        return 6; //Flush
+    }
+
+    //Return statemenet for straight
+    if(isStraight)
+    {
+        return 5; //Straight
+    }
+
+    //Check for three of a kind
+    for(int i = 0; i < 13; i++)
+    {
+        if(numCount[i] == 3)
+        {
+            return 4; //Three of a kind
+        }
+    }
+
+    //Check for two pair
+    for(int i = 0; i < 13; i++)
+    {
+        if(numCount[i] == 2)
+        {
+            for(int j = i + 1; j < 13; j++) //Iterates at i + 1, because everything before i should've been checked already
+            {
+                if(numCount[j] == 2)
+                {
+                    return 3; //Two pair
+                }
+            }
+        }
+    }
+
+    //Check for pair. I think I could implement this within the two pair check but I just want to be safe and implement here
+    for(int i = 0; i < 13; i++)
+    {
+        if(numCount[i] == 2)
+        {
+            return 2; //Pair
+        }
+    }
+
+    //Returns a high card if no other hand types are found
+    return 1; //High card
 }
 
 //Sets the four boolean arrays to be true when the game starts so that the getCard function works correctly
@@ -271,7 +492,7 @@ long long setBet(long long bank, long long *pot)
 {
     system("cls");
     long long bet;
-    printf("You currently have $%d in your bank. There are $%d in the pot. How much money would you like to bet (Enter 0 or below to cancel your bet and stand): ", bank, *pot);
+    printf("You currently have $%d in your bank. There are $%d in the pot. How much money would you like to bet (Enter 0 to cancel your bet and stand): ", bank, *pot);
     scanf("%d", &bet);
 
     if(bet <= 0)
@@ -279,6 +500,11 @@ long long setBet(long long bank, long long *pot)
         printf("Player chose not to bet...");
         sleep(1);
         return bank;
+    }
+    while(bet > bank)
+    {
+        printf("You currently have $%d in your bank. There are $%d in the pot. How much money would you like to bet (Enter 0 or below to cancel your bet and stand): ", bank, *pot);
+        scanf("%d", &bet);
     }
 
     bank -= bet;
@@ -288,6 +514,23 @@ long long setBet(long long bank, long long *pot)
     sleep(1);
 
     return bank;
+}
+
+void printWinner(int playerHandType, int dealerHandType, int playerHand[], int dealerHand[])
+{
+
+    if(playerHandType > dealerHandType)
+    {
+        //Function to print winner with what hand type
+    }
+    else if(dealerHandType > playerHandType)
+    {
+
+    }
+    else if(dealerHandType == playerHandType)
+    {
+
+    }
 }
 
 
@@ -320,7 +563,7 @@ int playPoker(long long bank)
 
     if(bank < 50)
     {
-        printf("You need at least $50 to buy-in for poker! Come back when you've got more cash.");
+        printf("You need at least $50 to buy-in for poker! Come back when you've got more cash.\n");
         return bank;
     }
 
@@ -351,7 +594,6 @@ int playPoker(long long bank)
     //     printf("TABLE CARD NO: %d SUIT: %d\n", tableCardNo[i], tableCardSuit[i]);
     // }
 
-    //NEED TO MAKE A PRINT CARDS FUNCTION HERE
 
     int tableCardsRevealed = 3; // Start by revealing 3 cards
     int turnCount = 0;
@@ -400,16 +642,56 @@ while (turnCount < 3) //Should only run 3  times
     else if (turnCount == 2)
     {
         tableCardsRevealed = 5;
-        printf("\nDealer places a final card on the table...\n");
+        printf("\nDealer places the final card on the table...\n");
         sleep(1);
     }
     // Do NOT print anything if turnCount == 3 (all 5 cards already out)
     
 }
 
+//EVALUATE THE WINNER
+
+//Make four new arrays that combine the table cards with both the dealer card arrays and the player card arrays for easier evaluation
+int playerAllCardNum[7], playerAllCardSuit[7];
+int dealerAllCardNum[7], dealerAllCardSuit[7];
+
+//Fills the arrays with the player and dealers' cards and suits
+for(int i = 0; i < 2; i++)
+{
+    playerAllCardNum[i] = playerCardNo[i];
+    playerAllCardSuit[i] = playerCardSuit[i];
+
+    dealerAllCardNum[i] = dealerCardNo[i];
+    dealerAllCardSuit[i] = dealerCardSuit[i];
+}
+
+//Fills the rest of the arrays with the table cards
+for(int i = 0; i < 5; i++)
+{
+    playerAllCardNum[i + 2] = tableCardNo[i];
+    playerAllCardSuit[i + 2] = tableCardSuit[i];
+
+    dealerAllCardNum[i + 2] = tableCardNo[i];
+    dealerAllCardSuit[i + 2] = tableCardSuit[i];
+}
+
+
+int playerHandType = evaluateHands(playerAllCardNum, playerAllCardSuit);
+int dealerHandType = evaluateHands(dealerAllCardNum, dealerAllCardSuit);
+
+printAllCards(playerCardNo, playerCardSuit, tableCardNo, tableCardSuit, tableCardsRevealed, dealerCardNo, dealerCardSuit);
+
+printf("%d \n", playerHandType);
+printf("%d \n", dealerHandType);
+
+printWinner(playerHandType, dealerHandType, playerCardNo, dealerCardNo);
+
+
 
 sleep(1);
 system("cls");
+
+return bank;
 
 
 }
@@ -521,12 +803,16 @@ void gameChoiceSwitch(int bank)
     scanf("%d", &gameChoice);
     system("cls");
 
+
+    //Tried making a switch statement to account for these but it was being weird. Had to enter 0 multiple times over to quit
+    //This just works better
+    if(gameChoice <=  0 || gameChoice >= 5)
+    {
+        return;
+    }
+
     switch(gameChoice)
     {
-    case 0:
-        return;
-
-
     case 1:
 
         bank = pokerMenu(bank);
@@ -545,51 +831,13 @@ void gameChoiceSwitch(int bank)
 
         gameChoiceSwitch(bank);
 
-
-    default:
-        return;
-
     }
 
 }
 
 int main()
 {
-    int gameChoice; //Will be used in the switch statement
     long long bank = 1000; //Starting value for bank is $1000
 
-
     gameChoiceSwitch(bank);
-
-    // switch(gameChoice)
-    // {
-    // case 0:
-    //     return 0;
-
-
-    // case 1:
-
-    //     bank = pokerMenu(bank);
-    //     main();
-
-    // case 2:
-
-
-    //     main();
-
-    // case 3:
-
-    //     main();
-
-    // case 4:
-
-    //     main();
-
-
-    // default:
-    //     return 0;
-
-    // }
-
-    
 }
